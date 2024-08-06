5dbc20eaef86964ca8a5df4dcf9225003b21d569
Damon Black <damon.black@gmail.com>
2024-08-06 15:01:56 +0000

    footer changes

--- components/Footer.vue
+++ components/Footer.vue
@@ -3,18 +3,24 @@
 </script>
 
 <template>
-<footer class="bg-dblue reading text-white text-xs p-4">
-    <div class="flex leading-6 flex-col items-center justify-center mt-8">
+<footer class="bg-dblue reading text-white text-xs">
+    <div class="flex leading-6 flex-col items-center justify-center my-8">
 <div>
-	    <div class="border-dashed border-white border-b text-center pb-4">
+	    <div class="m-4 pb-4 border-dashed border-white border-b text-center">
 	    <p>We acknowledge the Traditional Custodians upon the land upon which we live and work, and pay our respects, to their Elders past present and emerging.</p>
     <p>We recognise the practice of intergenerational care for Country and it's relevance to our work</p>
     <p>We see to listen, learn, and build strong relationships</p>
 	    </div>      
-    <div class="my-8 w-2/5 md:w-1/5">
-                <img class='mx-auto' src="/logos.svg" />
-            </div>
     </div>
+    <div class="my-8 pt-8 flex items-center mr-6">
+	    <div class="mr-6 w-5/6">
+		<img src="/CDS-Vic_Arrow_RGB.png" />
+
+	   </div>
+	    <div class="ml-2 flex justify-center">
+		<img class="w-1/2" src="/logos.svg" />
+	   </div>
+	   </div>
     </div>
  </footer>
 </template>
